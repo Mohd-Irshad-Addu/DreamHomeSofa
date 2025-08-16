4fdf0d97c9b13c8d7d61b6f68d6fc02ef2fca683
Mohd Irshad <Mohd-Irshad-Addu@users.noreply.github.com>
2025-08-16 09:35:32 +0530

    deploying for test

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,21 @@
-import { useState } from "react";
-import "./App.css";
+// App.jsx
+import { Routes, Route } from "react-router-dom";  // HashRouter hata diya
 import { Home, AboutUs, ContactUs, Services, Gallery } from "./pages";
-import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div className="min-h-screen bg-white">
-        {/* <h1 className="text-4xl font-bold text-center text-blue-600 mt-10"></h1> */}
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="services" element={<Services />} />
-            <Route path="gallery" element={<Gallery />} />
-            <Route path="contact" element={<ContactUs />} />
-            <Route path="about" element={<AboutUs />} />
-          </Route>
-        </Routes>
-      </div>
-    </>
+    <div className="min-h-screen bg-white">
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="services" element={<Services />} />
+          <Route path="gallery" element={<Gallery />} />
+          <Route path="contact" element={<ContactUs />} />
+          <Route path="about" element={<AboutUs />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { HashRouter } from "react-router-dom";
 import "./index.css";
-import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HashRouter>
       <App />
-    </StrictMode>
-  </BrowserRouter>
+    </HashRouter>
+  </React.StrictMode>
 );
